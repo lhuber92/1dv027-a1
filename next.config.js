dae3162f293edd3916d8c5452fe3131f06a146f1
lhuber92 <leo.huber1@gmail.com>
2022-03-22 21:08:42 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const nextConfig = {
-  basePath: '/1dv027-a1',
-  images: {
-    domains: ['hippohelp-logic-staging.shimmershot.com'],
-    path: '/1dv027-a1/_next/image',
-  }
+  basePath: '/1dv027-a1'
 }
 
 module.exports = nextConfig
