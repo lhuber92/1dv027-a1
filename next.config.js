3151e74bd1dd9a43688a70f1803dddd2724009f7
lhuber92 <leo.huber1@gmail.com>
2022-03-22 20:34:01 +0100

    a

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const nextConfig = {
-  basePath: '/1dv027-a1',
+  // basePath: '/1dv027-a1',
+  images: {
+    domains: ['hippohelp-logic-staging.shimmershot.com']
+  }
 }
 
 module.exports = nextConfig
