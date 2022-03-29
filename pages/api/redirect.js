26381918f0e6894bbfcb92974efb4c4c1db8dc68
lhuber92 <leo.huber1@gmail.com>
2022-03-29 20:08:53 +0200

    fix

--- pages/api/redirect.js
+++ pages/api/redirect.js
@@ -13,15 +13,12 @@ import errorThrower from '../../serverUtils/errorThrower.js';
  */
 export default async function handler(req, res) {
   try {
-    console.log('ddddddddddddddddddddddddddd')
     // Get the access token, with help of "code"
     let accessTokenResponse = await fetch(
       `https://gitlab.lnu.se/oauth/token?client_id=${process.env.APP_ID}&client_secret=${process.env.APP_SECRET}&code=${req.query.code}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT_URL}`,
       { method: 'POST' }
     )
     accessTokenResponse = await accessTokenResponse.json()
-    console.log('aaaaaaaaaaaaaaaa')
-    console.log(accessTokenResponse)
     errorThrower(accessTokenResponse)
     
     // Get user details with help of the id_token provided in accessTokenResponse
@@ -29,10 +26,8 @@ export default async function handler(req, res) {
     const username = userDetails.email.substring(0, 7)
     
     cookieSetter(req, res, accessTokenResponse.access_token, username)
-    res.end(307, process.env.BASE_URL) 
+    res.redirect(307, process.env.BASE_URL) 
   } catch (error) {
-    console.log('bbbbbbbbb')
-    console.log(error)
     errorSender(error, req, res)
   }
 }
