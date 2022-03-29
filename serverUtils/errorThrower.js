852908b79c3218022df03905b3ef5534860fac1a
lhuber92 <leo.huber1@gmail.com>
2022-03-29 20:54:08 +0200

    fixes

--- serverUtils/errorThrower.js
+++ serverUtils/errorThrower.js
@@ -5,6 +5,7 @@
  */
 const errorThrower = (response) => {
   // Gitlab api error
+  console.log(response)
   if (response?.message === '401 Unauthorized') { throw new Error(userdataResponse.message) }
 }
 
