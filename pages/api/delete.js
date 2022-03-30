157dab5590b7fc5f8f21e7b0cda00d0a3013fcd6
lhuber92 <leo.huber1@gmail.com>
2022-03-30 10:42:50 +0200

    as

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -16,7 +16,12 @@ export default async function handler (req, res) {
     const username = cookies.get('username')
     if (!username) { throw new Error() }
 
-    const localImagePath = `./public/uploads/${username}.jpeg`
+    // const localImagePath = `./public/uploads/${username}.jpeg`
+    // For demo purposes only. NextJS recommends a third party service for image storage since uploaded images are only
+    // visible after the app is rebuilt (npm run build)
+    // More info here: https://github.com/vercel/next.js/pull/17203/commits/572345f0d59fbfa30fa7538b0cffd69e152959d6
+    // And in my long comment in api/upload.js
+    const localImagePath = './public/uploads/default.jpeg'
     await fs.promises.unlink(localImagePath, fs.constants.F_OK) // Delete file
     .then(() => { res.status(200).end() })
     .catch((error) => { throw error })
